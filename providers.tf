b813521e10cf3c5380c00474ac53723235550099
MadhuSegu2022 <madhu.segu@gmail.com>
2024-10-02 21:38:44 +0200

    added credentials

--- providers.tf
+++ providers.tf
@@ -14,8 +14,9 @@ terraform {
     }
   }
 }
-#added comments
 provider "aws" {
   # Configuration options
-  region = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "access_key" {
+  description = "The AWS access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The AWS secret access key"
+  type        = string
+}
+
+variable "region" {
+  description = "The AWS region to deploy to"
+  type        = string
+}
